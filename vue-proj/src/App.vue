161cc89239f8c9f069166b403e6f4d5f9b920c0c
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-03-30 19:46:24 -0300

    first commit and few changes

---
+++ vue-proj/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   
+    <First />
+  </div>
+</template>
+
+<script>
+import First from './components/First.vue'
+
+export default {
+  name: 'app',
+  components: {
+    First
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
