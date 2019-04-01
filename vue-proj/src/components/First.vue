f72935ffd337f5fd9d7da3f94dce988389d917c2
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-04-01 12:10:35 -0300

    adding styling and logic to shoe the data array

--- vue-proj/src/components/First.vue
+++ vue-proj/src/components/First.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="hello">
    This is simple 
+
+
+   <div class="holder">
+     <ul>
+       <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+     </ul>
+   </div>
    <!-- class binding -->
    <!-- <div v-bind:class="{class_name_alert: var_in_data, 'another_class': !another_var}"></div> -->
 
@@ -14,6 +21,11 @@ export default {
   name: 'First',
   data(){
     return {
+      skills: [
+        {'skill': "Python"},
+        {'skill': "MEAN"},
+        {'skill': "VUE.js"},
+      ],
       // class binding use below
       // var_in_data: true,
       // another_var: true
@@ -30,14 +42,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  scoped>
-  .class_name_alert{
-    background-color: aqua;
-    width: 100%;
-    height: 20rem
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+  .holder {
+    background: #fff;
   }
-  .another_class{
-    background-color: blueviolet;
-    width: 100%;
-    height: 50rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
   }
 </style>
