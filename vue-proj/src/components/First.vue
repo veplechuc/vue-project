22eee17957f444b723788e6d9a03c022e3ea6d75
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-04-01 11:52:00 -0300

    multiple class binding.

--- vue-proj/src/components/First.vue
+++ vue-proj/src/components/First.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="hello">
    This is simple 
+   <div v-bind:class="{class_name_alert: var_in_data, 'another_class': !another_var}"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'First',
-  props: {
-  
+  data(){
+    return {
+      var_in_data: true,
+      another_var: true
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style  scoped>
+  .class_name_alert{
+    background-color: aqua;
+    width: 100%;
+    height: 20rem
+  }
+  .another_class{
+    background-color: blueviolet;
+    width: 100%;
+    height: 50rem;
+  }
 </style>
